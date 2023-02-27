f09f1fa71de6d8f4a9d8d33a3e13916ca4fdf37b
Zhenghua Chen <55592965+Zhenghua-404@users.noreply.github.com>
2023-02-26 19:12:21 -0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'https://absorbing-haircut-75a.notion.site/Zhenghua-s-Home-Page-cfe58e7290ad40548513284c76fbb5b7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Next.js Notion Personal Homepage',
+  domain: 'nextjs-notion-homepage.zhenghuachen.it',
+  author: 'Zhenghua Chen',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
